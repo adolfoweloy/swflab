3cedcf1272056daf88de6a8275338cb9693a46fa
adolfoweloy <1496547+adolfoweloy@users.noreply.github.com>
2025-01-19 20:36:31 +1100

    Model the mutable ActivityBase class suggested from AWS SWF guides (in Ruby)

---
+++ src/main/java/com/adolfoeloy/swflab/swf/domain/ActivityBase.java
@@ -0,0 +1,16 @@
+package com.adolfoeloy.swflab.swf.domain;
+
+/**
+ * Starting with the design inspired by the development guide from AWS SWF
+ * <a href="https://docs.aws.amazon.com/amazonswf/latest/developerguide/swf-sns-tutorial-implementing-activities.html">Implementing Activities</a>
+ * The design is horrible imo given the introduced mutability in this class.
+ * TODO: Make this all immutable pls!
+ */
+public class ActivityBase {
+    private String results;
+
+    public boolean doActivity(Task task) {
+        results = task.input();
+        return true;
+    }
+}
---
+++ src/main/java/com/adolfoeloy/swflab/swf/domain/Task.java
@@ -0,0 +1,4 @@
+package com.adolfoeloy.swflab.swf.domain;
+
+public record Task(String input) {
+}
