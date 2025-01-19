ced5afa8798d0f3091a7d5c737fc7510fd9d324a
adolfoweloy <1496547+adolfoweloy@users.noreply.github.com>
2025-01-19 18:01:51 +1100

    Add more details about task lists

--- src/main/java/com/adolfoeloy/swflab/swf/service/WorkflowInitializerService.java
+++ src/main/java/com/adolfoeloy/swflab/swf/service/WorkflowInitializerService.java
@@ -54,12 +54,18 @@ public class WorkflowInitializerService {
     }
 
     private Workflow registerWorkflow(Domain domain, String workflowName, UUID version, List<Activity> activities) {
-        var taskList = TaskList.builder().name(workflowProperties.decisionTaskList()).build();
+        var defaultTaskList = "default-" + workflowProperties.decisionTaskList();
+        var taskList = TaskList.builder().name(defaultTaskList).build();
         var registerRequest = RegisterWorkflowTypeRequest.builder()
                 .domain(domain.name())
                 .name(workflowName)
                 .version(version.toString())
+
+                // default task list for executions of this workflow type being registered
+                // a task list can be interpreted as a dynamic queue in the sense that I don't need to register them.
+                // this default task list will be used if a task list is not provided when starting a workflow.
                 .defaultTaskList(taskList)
+
                 .defaultChildPolicy(ChildPolicy.TERMINATE)
                 .defaultTaskStartToCloseTimeout(Integer.valueOf(3600).toString())
                 .defaultExecutionStartToCloseTimeout("3600")
