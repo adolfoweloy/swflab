4be111018cb16590f09b0d0ffc1bbee8c9c6631e
adolfoweloy <1496547+adolfoweloy@users.noreply.github.com>
2025-01-28 09:44:42 +1100

    Add Guava and AsserJ for testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,12 +20,15 @@ repositories {
 dependencies {
 	implementation(platform("software.amazon.awssdk:bom:2.29.30"))
 	implementation("org.apache.commons:commons-lang3:3.0")
+	implementation("com.google.guava:guava:33.4.0-jre")
 
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("software.amazon.awssdk:swf")
 	implementation("software.amazon.awssdk:sns")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testCompileOnly("org.assertj:assertj-core:3.11.1")
+
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
