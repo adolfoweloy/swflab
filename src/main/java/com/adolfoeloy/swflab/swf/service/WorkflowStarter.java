802150360e85a9d99baec906e1cb0422690b2af1
adolfoweloy <1496547+adolfoweloy@users.noreply.github.com>
2025-02-01 17:16:50 +1100

    Start activity poller from workflow starter

--- src/main/java/com/adolfoeloy/swflab/swf/service/WorkflowStarter.java
+++ src/main/java/com/adolfoeloy/swflab/swf/service/WorkflowStarter.java
@@ -1,5 +1,6 @@
 package com.adolfoeloy.swflab.swf.service;
 
+import com.adolfoeloy.swflab.swf.domain.activity.ActivitiesPoller;
 import com.adolfoeloy.swflab.swf.domain.activity.ActivityTypes;
 import com.adolfoeloy.swflab.swf.domain.decider.Decider;
 import com.adolfoeloy.swflab.swf.domain.decider.DecisionTaskHistoryEventsHandler;
@@ -20,17 +21,19 @@ public class WorkflowStarter {
     private final SwfClient swfClient;
     private final ExecutorService executor = Executors.newSingleThreadExecutor();
     private final DecisionTaskHistoryEventsHandler decisionTaskHistoryEventsHandler;
-
-    public WorkflowStarter(SwfClient swfClient, Workflow workflow, ActivityTypes activityTypes, DecisionTaskHistoryEventsHandler decisionTaskHistoryEventsHandler) {
+    private final ActivitiesPoller activitiesPoller;
+    public WorkflowStarter(SwfClient swfClient, Workflow workflow, ActivityTypes activityTypes, DecisionTaskHistoryEventsHandler decisionTaskHistoryEventsHandler, ActivitiesPoller activitiesPoller) {
         this.swfClient = swfClient;
         this.workflow = workflow;
         this.activityTypes = activityTypes;
         this.decisionTaskHistoryEventsHandler = decisionTaskHistoryEventsHandler;
+        this.activitiesPoller = activitiesPoller;
     }
 
     public WorkflowExecution start(UUID workflowId) {
         var workflowExecution = workflow.startExecution(swfClient, workflowId);
         pollForDecisions(workflowExecution);
+        activitiesPoller.triggerPollingFor(workflowExecution.workflowId().toString());
         return workflowExecution;
     }
 
