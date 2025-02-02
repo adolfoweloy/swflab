edc5473427b35f4015abca2c822b0a0f2935fc3d
adolfoweloy <1496547+adolfoweloy@users.noreply.github.com>
2025-02-02 18:27:57 +1100

    Add spotless plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.4.0"
 	id("io.spring.dependency-management") version "1.1.6"
+	id("com.diffplug.spotless") version "7.0.2"
 }
 
 group = "com.adolfoeloy"
@@ -12,6 +13,30 @@ java {
 		languageVersion = JavaLanguageVersion.of(23)
 	}
 }
+
+spotless {
+	// limit format enforcement to just the files changed by this feature branch
+	ratchetFrom("origin/main")
+
+	format("misc") {
+		// define the files to apply `misc` to
+		target("*.gradle", ".gitattributes", ".gitignore")
+
+		// define the steps to apply to those files
+		trimTrailingWhitespace()
+		leadingTabsToSpaces()
+		endWithNewline()
+	}
+
+	java {
+		// apply a specific flavor of google-java-format
+		palantirJavaFormat()
+
+		// fix formatting of type annotations
+		formatAnnotations()
+	}
+}
+
 
 repositories {
 	mavenCentral()
@@ -26,6 +51,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("software.amazon.awssdk:swf")
 	implementation("software.amazon.awssdk:sns")
+	implementation("com.diffplug.spotless:spotless-plugin-gradle:6.8.0")
 
 	runtimeOnly("org.postgresql:postgresql")
 
